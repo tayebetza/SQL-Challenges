b03b885011af05f8739ebd80d088297c2715b6c4
tayebetza <bezataye99@gmail.com>
2025-01-23 17:17:44 +0000

    PD 2023 Week2 Sol

---
+++ PreppinData/Week2-2023/Week2-2023.sql
@@ -0,0 +1,6 @@
+-- SQLite
+select
+replace(sort_code,'-','') as "Sort Code"
+from TIL_PLAYGROUND.PREPPIN_DATA_INPUTS.PD2023_WK02_SWIFT_CODES s
+inner join TIL_PLAYGROUND.PREPPIN_DATA_INPUTS.PD2023_WK02_TRANSACTIONS t
+    on s.bank= t.bank
